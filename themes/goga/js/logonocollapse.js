e554100993a5e6b2372731eeb04d5b87643b04b5
GordanaRajnjak <gogashkk@gmail.com>
2017-07-18 13:40:15 +0200

    First commit

---
+++ themes/goga/js/logonocollapse.js
@@ -0,0 +1,16 @@
+/*
+$('.logo').on('click', function (e) {
+    e.stopPropagation();
+});
+*/
+
+/*
+
+$(".logo a").click(function (e) {
+    return false;
+});
+
+
+*/
+/*
+$("#logo").parent().removeClass("collapse");
